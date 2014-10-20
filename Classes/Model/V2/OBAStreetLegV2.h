32d6fab234dd43dc2892c1ee852a42a27340e294
Caitlin Bonnar <cbonnar@cs.washington.edu>
2014-10-20 14:23:23 -0700

    Deprecate onebusaway-iphone-common
    See README

--- Classes/Model/V2/OBAStreetLegV2.h
+++
@@ -1,9 +0,0 @@
-@interface OBAStreetLegV2 : NSObject {
-    
-}
-
-@property (nonatomic,retain) NSString * streetName;
-@property (nonatomic,retain) NSString * path;
-@property (nonatomic) double distance;
-
-@end
